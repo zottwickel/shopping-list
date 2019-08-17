c9c51203d76cc2e90232714e4650eada50dccfd5
zottwickel <scott.g.mckell@gmail.com>
2019-08-17 14:09:35 -0600

    debug toggle and delete functionality

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-$('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
-    let listItem = $(this.parent().parent().parent().children('.shopping-item'))
-    if (listItem.hasClass('shopping-item__checked')) {
-        listItem.removeClass('shopping-item__checked');
-    } else {
-        listItem.addClass('shopping-item__checked');
-    }
-});
+$(function () {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
+        e.stopPropagation();
+        let element = $($(e.target).parents('li').get()).children('.shopping-item').get(0);
+        if ($(element).hasClass('shopping-item__checked')) {
+            $(element).removeClass('shopping-item__checked');
+        } else {
+            $(element).addClass('shopping-item__checked');
+        }
+    });
 
-$('.shopping-list').on('click', 'shopping-item-delete', function(e) {
-    $(this.parent().parent().parent()).remove();
-});
+    $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
+        e.stopPropagation();
+        let element = $(e.target).parents('li').get();
+        $(element).remove();
+    });
+})
